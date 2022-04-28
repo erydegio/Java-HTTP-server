adffea486c61d2e1c6cc3d6260eb9da325dfb708
erydegio <degiovannierica16@gmail.comgit remotegit config --global user.name erydegioclear>
2022-04-28 09:02:59 +0200

    fix stopServer divided tcp and http classes

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -1,28 +1,54 @@
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class HTTPServer extends TCPServer {
+// Multithreaded HTTPServer
+
+public class HTTPServer implements Runnable {
+
+    private final Integer port;
+    private Socket        socket = null;
+    private ServerSocket  server = null;
+    //String threadName = Thread.currentThread().getName();
+    //private boolean       running = false;
 
     public HTTPServer(Integer port) {
-        super(port);
+        this.port = port;
+    }
+
+    public void startServer() throws IOException {
+        // start listening for incoming connections on port 8080
+        server = new ServerSocket(port);
+        System.out.println("Server listening on port: " + port);
+        //running = true;
+    }
+
+    public void stopServer() throws IOException {
+        //running = false;
+        System.out.println("Closing the Server...");
+        //server.close();
+        Thread.currentThread().interrupt();
     }
 
     @Override
     public void run() {
 
-        while (running) try {
+        // main
+        try {
+            startServer();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) try {
 
             socket = server.accept();
-
             new Thread(new HTTPRequestHandler(socket)).start();
 
         } catch (IOException e) {
-            if (!running) {
-                System.out.println("Bye..");
-                return;
-            }
+           e.printStackTrace();
         }
     }
-
-
 
 }
